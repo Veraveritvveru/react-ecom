c5d716abf04b3264433d627bdb1fbe1a796ac184
Vera Krumpan <filimonovavera96@gmail.com>
2025-09-08 10:52:51 +0300

    feat: add getFilteredProducts in mainContent

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,138 @@
-// import { useEffect, useState } from 'react';
-// import { useFilter } from '../hooks/useFilter';
-// import { Tally3 } from 'lucide-react';
-// import productsData from '../products.json';
+import { useEffect, useState } from 'react';
+import { useFilter } from '../hooks/useFilter';
+import { Tally3 } from 'lucide-react';
+import productsData from '../products.json';
+import { Product } from '../interfaces';
 
 const MainContent = () => {
-  return <section></section>;
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } =
+    useFilter();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filter, setFilter] = useState('all');
+  const [currentPage] = useState(1);
+  const [drodownOpen] = useState(true);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [drodownOpen, setDropdownOpen] = useState(true);
+  const itemsPerPage = 12;
+
+  useEffect(() => {
+    let filtered = productsData.products;
+
+    if (keyword) {
+      filtered = filtered.filter((p) =>
+        p.title.toLowerCase().includes(keyword.toLowerCase())
+      );
+    }
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const paginated = filtered.slice(start, end);
+    console.log(typeof paginated);
+    setProducts(paginated);
+  }, [currentPage, keyword]);
+
+  const getFilteredProducts = () => {
+    let filteredProducts = products;
+
+    if (selectedCategory) {
+      filteredProducts = filteredProducts.filter((product) => {
+        return product.category === selectedCategory;
+      });
+    }
+
+    if (minPrice) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price >= minPrice
+      );
+    }
+
+    if (maxPrice) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price <= maxPrice
+      );
+    }
+
+    if (searchQuery) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    switch (filter) {
+      case 'expensive':
+        console.log(filteredProducts.sort((a, b) => b.price - a.price));
+        return filteredProducts.sort((a, b) => b.price - a.price);
+      case 'cheap':
+        console.log(filteredProducts.sort((a, b) => a.price - b.price));
+        return filteredProducts.sort((a, b) => a.price - b.price);
+      case 'rating':
+        console.log(filteredProducts.sort((a, b) => b.rating - a.rating));
+        return filteredProducts.sort((a, b) => b.rating - a.rating);
+      default:
+        return filteredProducts;
+    }
+  };
+
+  const filterProducts = getFilteredProducts();
+  console.log(filterProducts);
+
+  return (
+    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+      <div className="mb-5">
+        <div className="flex flex-col justify-between items-center sm:flex-row">
+          <div className="filter-wrapper relative mb-5 mt-5">
+            <button className="filter-btn border px-4 py-2 rounded-full flex items-center">
+              <Tally3 className="mr-2" />
+
+              {filter === 'all'
+                ? 'Filter'
+                : filter.charAt(0).toUpperCase() + filter.slice(1)}
+            </button>
+
+            {drodownOpen && (
+              <div className="choose-filter-buttons absolute bg-white border border-neutral-400 rounded mt-2 w-full sm:w-40">
+                <button
+                  className="block p-2 w-full text-left px-4 hover:bg-neutral-400"
+                  onClick={() => setFilter('cheap')}
+                >
+                  Cheap
+                </button>
+                <button
+                  className="block p-2 w-full text-left px-4 hover:bg-neutral-400"
+                  onClick={() => setFilter('expensive')}
+                >
+                  Expensive
+                </button>
+                <button
+                  className="block p-2 w-full text-left px-4 hover:bg-neutral-400"
+                  onClick={() => setFilter('rating')}
+                >
+                  Rating
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="book-list grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5">
+          {/* {Bookcard} */}
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-between items-center mt-5">
+          <button
+            disabled={currentPage === 1}
+            className="border px-4 py-2 mx-2 rounded-full"
+          >
+            Previous
+          </button>
+
+          <div className="flex flex-wrap justify-center"></div>
+
+          <button className="border px-4 py-2 mx-2 rounded-full">Next</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MainContent;
--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -10,3 +10,37 @@ export interface FilterContextType {
   keyword: string;
   setKeyword: (keyword: string) => void;
 }
+
+interface ProductReview {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand?: string | undefined;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: ProductReview[];
+  images: string[];
+  thumbnail: string;
+}
